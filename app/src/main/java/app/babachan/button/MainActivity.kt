4ed270b1ec42e1036ccf04d7f5076465f34358e1
lifeistech <[64596612+giraffecandy@users.noreply.github.com]>
2020-09-18 16:21:24 +0900

    Initial commit

---
+++ app/src/main/java/app/babachan/button/MainActivity.kt
@@ -0,0 +1,16 @@
+package app.babachan.button
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        fun click(view: View){
+
+        }
+    }
+}
